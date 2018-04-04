21d2b6b76292081c85a3263c6b2322d80477447a
Sobin <sobinmathew988@gmail.com>
2018-04-04 16:35:15 +0530

    initial commit

--- app/src/main/java/touch/in/keep/sobin/loginapp/LoginActivity.java
+++ app/src/main/java/touch/in/keep/sobin/loginapp/LoginActivity.java
@@ -77,7 +77,7 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent =new Intent(this,MainActivity.class);
             startActivity(intent);
         }else{
-            
+
         }
 
     }
--- app/src/main/java/touch/in/keep/sobin/loginapp/SplashScreen.java
+++ app/src/main/java/touch/in/keep/sobin/loginapp/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent =new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent =new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
                 finish();
             }
